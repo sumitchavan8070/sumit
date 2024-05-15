5a63b42d1eef2b04e12fae1d341baf7688598a60
sumitchavann <136459557+sumitchavann@users.noreply.github.com>
2024-05-15 18:04:14 +0530

    first commit

---
+++ apps/enji.dev/src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from '@/components/meta/Head';
+
+import { getBaseUrl } from '@/helpers/url';
+
+import IndexContents from '@/contents/index';
+
+function Index() {
+  return (
+    <>
+      <Head
+        title="Sumit Chavan · Flutter Developer"
+        description="An online portfolio featuring a showcase of my projects and some thoughts as a Flutter Developer who loves intuitive, clean and modern UI design."
+        ogImage={`${getBaseUrl()}/assets/images/og-image.png`}
+        overrideTitle
+      />
+      <IndexContents />
+    </>
+  );
+}
+
+export default Index;
